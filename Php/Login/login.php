cab11f3200a1ef73af60597a6e4aac438e98e3d0
Etienne Pro <formation.etienne.re@gmail.com>
2021-01-12 14:54:24 +0400

    nom a trouver

--- Php/Login/login.php
+++ Php/Login/login.php
@@ -11,16 +11,22 @@
 
 <body>
     <form action="login.php" method="post" >        
-            <input type="text" name="nom"  placeholder="Entrer votre nom:" required> 
-            <input type="email" name="email" placeholder="Entrer votre email:" >        
+            <input type="text" name="nomtapper"  placeholder="Entrer votre nom" required> 
+            <input type="password" name="mdptapper" placeholder="Entrer votre mot de passe" >        
             <input type="submit" value="S'inscrire!">        
     </form>
     <?php
-    if (isset($_POST['nom'])){
-        $nom = $_POST['nom'];
-        $email = $_POST['email'];
-        echo "Bienvenue $nom";
-        echo "<br> Un mail de confirmation a été envoyé à l'adressse $email";
+    $mdpatrouver = "123";
+    $nomatrouver = "test";
+    if (isset($_POST['nomtapper']) & isset($_POST['mdptapper'])){
+        $nomtapper = $_POST['nomtapper'];
+        $mdptapper = $_POST['mdptapper'];
+        if($mdptapper == $mdpatrouver & $nomtapper == $nomatrouver){
+            echo 'Authentification ok';
+        }
+        else{
+            echo 'Authentification pas ok';
+        }        
     }
     ?>
 </body>
